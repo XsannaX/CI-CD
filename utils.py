cb284e92f51907ec54cda6b716f21045ce2443b3
XsannaX <alexa060402@gmail.com>
2024-05-15 14:31:45 +0200

    added docstrings

--- utils.py
+++ utils.py
@@ -1,15 +1,19 @@
 # przykladowe funkcje utils .py
 def add(a: int, b: int) -> int:
+    """Comm"""
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
+    """Comm"""
     return a - b
 
 
 def multiply(a: int, b: int) -> int:
+    """Comm"""
     return a * b
 
 
 def divide(a: int, b: int) -> float:
+    """Comm"""
     return a / b
